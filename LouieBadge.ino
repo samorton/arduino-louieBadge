72c3ed7a5a4366104a69b527fe412949820a4c5f
Sam Orton <sam.orton@gmail.com>
2018-02-23 23:18:51 +0000

    many messages

--- LouieBadge.ino
+++ LouieBadge.ino
@@ -5,22 +5,31 @@
 #define CS 1
 #define WR 3
 //#define RD 8
+volatile int buttonState = 0;         
+
+#define BTN 2     
+
 
 DFRobot_HT1632C ht1632c = DFRobot_HT1632C(DATA, WR,CS);
 
-char str[] = " Hello   Louie :)";
+char* messages[] = {" Hello   Louie :)",
+               "Hi Mabel xx <3 x",
+               "Love you Lilia " };
 
 void setup() {
-
-  // put your setup code here, to run once:
+  attachInterrupt(0, pin_ISR, CHANGE);
+  pinMode(BTN, INPUT);
   ht1632c.begin();
   ht1632c.isLedOn(true);
   ht1632c.clearScreen();
   delay(500);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  ht1632c.print(str,50);
+  for (int i=0;i<3;i++){
+    ht1632c.print(messages[i],50);
+  }
   //delay(100);
 }
