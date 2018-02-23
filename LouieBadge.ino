7bb614469a367eb928e39c116384ca99689380df
Sam Orton <sam.orton@gmail.com>
2018-02-23 23:43:08 +0000

    interrupts working, text needs some work

--- LouieBadge.ino
+++ LouieBadge.ino
@@ -11,25 +11,35 @@ volatile int buttonState = 0;
 
 
 DFRobot_HT1632C ht1632c = DFRobot_HT1632C(DATA, WR,CS);
-
+int i =0;
 char* messages[] = {" Hello   Louie :)",
                "Hi Mabel xx <3 x",
                "Love you Lilia " };
 
 void setup() {
-  attachInterrupt(0, pin_ISR, CHANGE);
+  attachInterrupt(0, pin_ISR, RISING);
   pinMode(BTN, INPUT);
   ht1632c.begin();
   ht1632c.isLedOn(true);
   ht1632c.clearScreen();
   delay(500);
-  
 }
+char* currmsg;
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for (int i=0;i<3;i++){
-    ht1632c.print(messages[i],50);
-  }
+   ht1632c.inLowpower(false);
+  ht1632c.print(messages[i],5);
+  delay(100);
+  
+   ht1632c.inLowpower(true);
   //delay(100);
+}
+
+
+void pin_ISR() {
+  buttonState = digitalRead(BTN);
+  if (++i>2)
+    i=0;
+   
+  
 }
